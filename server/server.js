5d6c6148cd527f77cda7ddca2e47b68b8227e566
suraj-chhabra-3 <surajchhabra3333@gmail.com>
2025-08-10 15:54:30 +0530

    project

--- server/server.js
+++ server/server.js
@@ -1,24 +1,32 @@
-import express from 'express'
+import express, { application, json } from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
-import { clerkWebhooks } from './controllers/webhooks.js';
+import { clerkWebhooks, stripeWebHooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
+import connectCloudinary from './configs/cloudinary.js';
+import courseRouter from './routes/courseRoute.js';
+import userRouter from './routes/userRoutes.js';
 
 // initialize the server
 const app = express();
 
 // connect data-base
 await connectDB();
+await connectCloudinary();
 
 // middlewares
 app.use(cors());
+app.use(clerkMiddleware()); // add 'auth' property to the req object
 
 // routes 
-app.get('/', (req, res) => {
-    res.send("API is working sir.")
-})
-
+app.get('/', (req, res) =>  res.send("API is working sir.") )
 app.post('/clerk', express.json(), clerkWebhooks);
+app.use('/api/educator', express.json(), educatorRouter) 
+app.use('/api/course', express.json(), courseRouter);
+app.use('/api/user', express.json(), userRouter);
+app.post('/stripe', express.raw({type: 'application/json'}, stripeWebHooks))
 
 // port
 const PORT = process.env.PORT || 5000
